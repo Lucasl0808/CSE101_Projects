fa87b86321af2d004810fe1f4c0f38c7d0a11d9d
Lucas Lee <luclee@ucsc.edu>
2022-04-25 03:02:50 -0700

    working on structs

---
+++ pa4/Matrix.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "List.h"
+#include "Matrix.h"
+
+typedef struct EntryObj* Entry;
+
+typedef struct EntryObj{
+	int col;
+	double value;
+}EntryObj;
+
---
+++ pa4/Matrix.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "List.h"
+
+//define matrix struct here
+Matrix newMatrix(int n);
+void freeMatrix(Matrix* pM);
+
+int size(Matrix M);
+int equals(Matrix A, Matrix B);
+
+void makeZero(Matrix M);
+void changeEntry(Matrix M, int i , int j , double x);
+
+Matrix copy(Matrix A);
+Matrix scalarMult(double x, Matrix A);
+Matrix transpose(Matrix A);
+Matrix sum(Matrix A, Matrix B);
+Matrix diff(Matrix A, Matrix B);
+Matrix product(Matrix A, Matrix B);
+
+void printMatrix(FILE *out, Matrix M);
