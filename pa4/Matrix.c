58cead1efdd0ea0b82fae2fc7ad756650268d649
Lucas Lee <luclee@ucsc.edu>
2022-04-29 02:54:21 -0700

    working on changeEntry, finished copy

--- pa4/Matrix.c
+++ pa4/Matrix.c
@@ -37,7 +37,7 @@ void freeEntry(Entry *pE){
 Matrix newMatrix(int n){
 	Matrix M = malloc(sizeof(MatrixObj));
 	M->Matrix = malloc((n + 1) * sizeof(List));
-	for(int i = 1; i > (n+1); i += 1){
+	for(int i = 1; i < (n+1); i += 1){
 		M->Matrix[i] = newList();
 	}
 	M->elements = 0;
@@ -47,7 +47,7 @@ Matrix newMatrix(int n){
 
 void freeMatrix(Matrix *pM){
 	if(pM != NULL && *pM != NULL){
-		for(int i = 1; i > size(*pM) + 1; i += 1){
+		for(int i = 1; i < size(*pM) + 1; i += 1){
 			//free entries, then free lists
 			moveFront((*pM)->Matrix[i]);
 			while(index((*pM)->Matrix[i]) >= 0){
@@ -67,7 +67,7 @@ void freeMatrix(Matrix *pM){
 
 int size(Matrix M){
 	if(M == NULL){
-		printf("Matrix Error: size() on a NULL Matrix");
+		printf("Matrix Error: size() on a NULL Matrix\n");
 		exit(EXIT_FAILURE);
 	}
 	return M->size;
@@ -75,7 +75,7 @@ int size(Matrix M){
 
 int NNZ(Matrix M){
 	if(M == NULL){
-		printf("Matrix Error: NNZ() on a NULL Matrix");
+		printf("Matrix Error: NNZ() on a NULL Matrix\n");
 		exit(EXIT_FAILURE);
 	}
 	return M->elements;
@@ -84,13 +84,13 @@ int NNZ(Matrix M){
 //false = 0, true = 1
 int equals(Matrix A, Matrix B){
 	if(A == NULL || B == NULL){
-		printf("Matrix Error: equals() on a NULL Matrix");
+		printf("Matrix Error: equals() on a NULL Matrix\n");
 		exit(EXIT_FAILURE);
 	}
-	if(size(A) != size(B)){
+	if(size(A) != size(B) || NNZ(A) != NNZ(B)){
 		return 0;
 	}
-	for(int i = 1; i < size(A) + 1; i += 1){
+	for(int i = 1; i < (size(A) + 1); i += 1){
 		List AL = A->Matrix[i];
 		List BL = B->Matrix[i];
 		moveFront(AL);
@@ -119,6 +119,55 @@ void makeZero(Matrix M){
 			moveNext(M->Matrix[i]);
 		}
 	}
+	M->elements = 0;
 }
 
+void changeEntry(Matrix M, int i, int j, double x){
+	if(M == NULL){
+		printf("Matrix Error: changeEntry() on a NULL Matrix\n");
+		exit(EXIT_FAILURE);
+	}
+	if(i < 1 || i > size(M)){
+		printf("Matrix Error: changeEntry() invalid i value (row)\n");
+		exit(EXIT_FAILURE);
+	}
+	if(j < 1 || j > size(M)){
+		printf("Matrix Error: changeEntry() invalid j value (column)\n");
+		exit(EXIT_FAILURE);
+	}
+	//M->Matrix[i] = Current list
+	//if x is 0, remove an entry, do with: if x = 0 ... else append entry
+	//if column entry already exists, replace value, dont increment M->elements
+	//iterate through M->Matrix[i], if list is empty, append new entry
+	//otherwise, insert entry in sorted column order
+	//increment M->elements when done, unless x is 0, then decrement
+}
 
+Matrix copy(Matrix A){
+	Matrix M = newMatrix(size(A));
+	for(int i = 1; i < size(A) +1; i += 1){
+		List L = A->Matrix[i];
+		if(length(L) == 0){
+			continue;
+		}
+		moveFront(L);
+		while(index(L) >= 0){
+			Entry E = get(L);
+			//Entry N = newEntry(E->col, E->value);
+			//append(M->Matrix[i], N);
+			changeEntry(M, i, E->col, E->value);
+			moveNext(L);
+		}
+	}
+	return M;
+}
+
+int main(void){
+	Matrix M = newMatrix(10);
+	Matrix R = newMatrix(10);
+	if(equals(M, R) == 1){
+		printf("Matrices are equal");
+	}
+	freeMatrix(&R);
+	freeMatrix(&M);
+}
