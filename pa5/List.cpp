be14223eceab15802df8ec3a818c03301faad97c
Lucas Lee <luclee@ucsc.edu>
2022-05-05 01:39:18 -0700

    finished constructor/destructor

--- pa5/List.cpp
+++ pa5/List.cpp
@@ -10,12 +10,35 @@ List::Node:Node(ListElement x){
 }
 
 List::List(){
-	frontDummy = nullptr;
-	backDummy = nullptr;
-	beforeCursor = nullptr;
-	afterCursor = nullptr;
+	frontDummy = new Node(-1);
+	backDummy = new Node(-1);
+	frontDummy->next = backDummy;
+	backDummy->prev = frontDummy;
+	beforeCursor = frontDummy;
+	afterCursor = backDummy;
 	pos_cursor = -1;
 	num_elements = 0;
 }
 
+List::List(const List& L){
+	frontDummy = new Node(-1);
+	backDummy = new Node(-1);
+	frontDummy->next = backDummy;
+	backDummy->prev = frontDummy;
+	beforeCursor = frontDummy;
+	afterCursor = backDummy;
+	pos_cursor = -1;
+	num_elements = 0;
+	
+	Node* N = L.front;
+	while(N != nullptr){
+		this->insertBefore(N->data);
+		N = N->next;
+	}
+}
 
+List::~List(){
+	clear();
+	delete frontDummy;
+	delete backDummy;
+}
