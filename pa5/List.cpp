d4ea9497c8135c913d1455b5b7738e03b318148e
Lucas Lee <luclee@ucsc.edu>
2022-05-05 10:22:04 -0700

    finished access functions for List.cpp

--- pa5/List.cpp
+++ pa5/List.cpp
@@ -16,7 +16,7 @@ List::List(){
 	backDummy->prev = frontDummy;
 	beforeCursor = frontDummy;
 	afterCursor = backDummy;
-	pos_cursor = -1;
+	pos_cursor = 0;
 	num_elements = 0;
 }
 
@@ -27,7 +27,7 @@ List::List(const List& L){
 	backDummy->prev = frontDummy;
 	beforeCursor = frontDummy;
 	afterCursor = backDummy;
-	pos_cursor = -1;
+	pos_cursor = 0;
 	num_elements = 0;
 	
 	Node* N = L.front;
@@ -42,3 +42,41 @@ List::~List(){
 	delete frontDummy;
 	delete backDummy;
 }
+
+int List::length() const{
+	return(num_elements);
+}
+
+ListElement List::front() const{
+	if(num_elements <= 0){
+		throw std::length_error("List: front(): empty List");
+	}
+	return (frontDummy->next->data);
+}
+
+ListElement List::back() const{
+	if(num_elements <= 0){
+		throw std::length_error("List: back(): empty List");
+	}
+	return (backDummy->prev->data);
+}
+
+int List::position() const{
+	return(pos_cursor);
+}
+
+ListElement List::peekNext() const{
+	if(pos_cursor >= num_elements){
+		throw std::length_error("List: peekNext(): cursor at end of List");
+	}
+	return(afterCursor->data);
+}
+
+ListElement List::peekPrev() const{
+	if(pos_cursor <= 0){
+		throw std::length_error("List: peekPrev(): cursor at beginning of List");
+	}
+	return(beforeCursor->data);
+}
+
+
