774dc8b542580f881e48976ce10e265b26fd8e5b
Lucas Lee <luclee@ucsc.edu>
2022-06-01 03:28:53 -0700

    adding readme and update dictionarytest

--- pa8/DictionaryTest.cpp
+++ pa8/DictionaryTest.cpp
@@ -31,59 +31,6 @@ int main(void){
         if(A == B){
                 std::cout << "A = B" << std::endl;
         }
-	std::string P[] = {"plaices",
-                     "ambusher",
-                     "crosby",
-                     "wattles",
-                     "pardoner",
-                     "pythons",
-                     "second",
-                     "forms",
-                     "impales",
-                     "pic",
-                     "verticals",
-                     "recriminator",
-                     "depressants",
-                     "cul",
-                     "potables",
-                     "norm",
-                     "reinsurer",
-                     "deschooler",
-                     "recoining",
-                     "dissocialised",
-                     "cohabit",
-                     "hemiolia",
-                     "cantling",
-                     "glamorized",
-                     "millesimal",
-                     "glagolitic"};
-	Dictionary C;
-	for(int i = 0; i < 26; i ++){
-		C.setValue(P[i], i +1);
-		std::cout << C << std::endl;
-	}
-	std::cout << C.size() << std::endl;
-	std::cout << C << std::endl;
-	
-	std::string O[] = {"second",
-                     "forms",
-                     "impales",
-                     "pic",
-                     "verticals",
-                     "recriminator",
-                     "depressants",
-                     "cul",
-                     "potables",
-                     "norm",
-                     "reinsurer",
-                     "deschooler",
-                     "recoining",      
-                  };
-
-	for(int i = 0; i < 13; i ++){
-		C.remove(O[i]);
-	}
-	std::cout << C << std::endl;
 }
 
 
---
+++ pa8/README.md
@@ -0,0 +1,13 @@
+# pa8 - Word Frequency and Red Black Trees
+This assignment is a continuation of pa7, and uses a dictionary ADT with red black trees
+
+## Files
+Makefile - creates and deletes executables, as well as compiles files<br>
+Dictionary.h - includes the function declarations for the Dictionary ADT<br>
+Dictionary.cpp - includes the ADT definitions and access/manipulation procedures<br>
+DictionaryTest.cpp - Test file for Dictionary ADT <br>
+Order.cpp - takes infile of words and sorts them into an outfile<br>
+WordFrequency.cpp - takes infile and sorts words to an outfile with the frequency of each word listed next to it<br>
+
+## comments
+Makefile test is changed a bit
